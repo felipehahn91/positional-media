436628bd898daa2e18b2f19a00613a54477cb57d
[dyad] <felipehah@gmail.com>
2025-09-15 13:47:21 -0300

    [dyad] Diminuído o logo do cabeçalho - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,11 +34,11 @@ const Header = () => {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-56">
+        <div className="flex items-center justify-between h-28">
           {/* Logo */}
           <div className="flex-shrink-0">
             <a href="/">
-              <img src={logo} alt="Positional Media" className="h-48 w-auto" />
+              <img src={logo} alt="Positional Media" className="h-24 w-auto" />
             </a>
           </div>
 
